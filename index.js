02b95e08d3bf5475dc21527c66995ecb0b386a8e
mddengo@gmail.com <indigo@Mes-Macbook-Pro.wv.cc.cmu.edu>
2018-07-17 14:53:59 -0400

    instructor/other login branches

--- index.js
+++ index.js
@@ -1,55 +1,99 @@
-// Vue.component('tab-delegation-log', {
-//    template: '<div class="tab-content">Delegation Log</div>',
-// });
-//
-// Vue.component('tab-active-modules', {
-//     template: '<div class="tab-content">Active modules</div>',
-// });
-// Vue.component('tab-pending-peer-reviews', {
-//     template: '<div class="tab-content">Pending peer reviews</div>',
-// });
+var store = {
+    debug: false,
+    state: {
+        isInstructor: false,
+        loggedIn: false,
+    },
+    setInstructor () {
+        this.state.isInstructor = true;
+        if (this.debug) console.log('setMessageAction triggered with', this.state.isInstructor)
+    },
+    clearInstructor () {
+        if (this.debug) console.log('clearMessageAction triggered')
+        this.state.isInstructor = false;
+    },
+    setLoggedIn () {
+        this.state.loggedIn = true;
+    },
+    clearLoggedIn () {
+        this.state.loggedIn = false;
+    },
 
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//         currTab: 'Delegation Log',
-//         tabs: ['Active Modules', 'Pending Peer Reviews', 'Delegation Log'],
-//     },
-//     computed: {
-//         currTabComponent: function() {
-//             let currTab = this.currTab.toLowerCase().split(' ').join('-');
-//             return 'tab-' + currTab;
-//         }
-//     }
-// });
+};
 
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-// Vue.component('my-page', {
-//     template: '#my-page'
-// });
+const Home = {
+    template: `
+        <div v-if="isInstructor" class="instructor">
+            <h3>Instructor Dashboard</h3>
+            <p>{{this.isInstructor}}</p>
+        </div>
+        
+        <div v-else class="student">
+            <h3>Course Description</h3>
+            <p>{{this.isInstructor}}!</p>
+            <q-btn color="indigo-11" label="START"></q-btn>
+        </div>
+    `,
+    data: function () {
+        return {
+            isInstructor: store.state.isInstructor,
+        }
+    },
+    // ready: function () {
+    //     store.setLoggedIn();
+    // }
+};
+const Login = {
+    data: function () {
+        return {
+            user: '',
+        }
+    },
+    template: `
+        <div class="login">
+            <div style="width:300px; max-width:70vw;">
+                <q-input v-model="user" float-label="Username" />
+                <q-input type="password" float-label="Password" />
+                <div style="position:absolute; left:40%; margin-top:15px">
+                    <router-link to="/"><q-btn color="teal" label="Login"></q-btn></router-link>
+                </div>
+            </div>
+        </div>
+    `,
+    watch: {
+        user(val) {
+            if (val === 'instructor') {
+                store.setInstructor();
+            } else {
+                store.clearInstructor();
+            }
+        },
+    },
+    // ready: function () {
+    //     store.clearLoggedIn();
+    // }
+};
 
 const routes = [
-    { path: '/foo', name: 'Foo', component: Foo },
-    { path: '/bar', name: 'Bar', component: Bar },
-]
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/', name: 'Home', component: Home },
+];
 const router = new VueRouter({
     routes
-})
+});
 
 const app = new Vue({
     el: '#app',
     router,
-    // data: function () {
-    //     return {
-    //         version: Quasar.version,
-    //         drawerState: true
-    //     }
-    // },
-    // methods: {
-    //     launch: function (url) {
-    //         Quasar.utils.openURL(url)
-    //     }
-    // },
+    data: {
+        loggedIn: store.state.loggedIn,
+    },
+    methods: {
+    },
+    watch: {
+    },
+    components: {
+        login: Login,
+        home: Home,
+    }
 }).$mount('#app')
